ea4672637669c51a96fb6f4b75b0c3cfdb02ddc6
akameco <akameco.t@gmail.com>
2016-03-26 23:04:41 +0900

    Support tag

--- cli.js
+++ cli.js
@@ -8,16 +8,29 @@ const cli = meow(`
 	Usage
 	  $ qiita-home [name]
 	  $ qh [name]
+	Options
+	  -t, --tag  Search tags
 	Examples
 	  $ qiita-home
 	  $ qiita-home akameco
-`);
+	  $ qiita-home -t Node.js
+`, {
+	alias: {
+		t: 'tag'
+	}
+});
 
 function open(name) {
 	opn(`http://qiita.com/${name}`, {wait: false});
 }
 
-if (cli.input.length > 0) {
+function openTag(tag) {
+	opn(`http://qiita.com/tags/${tag}`, {wait: false});
+}
+
+if (cli.flags.tag) {
+	openTag(cli.flags.tag);
+} else if (cli.input.length > 0) {
 	open(cli.input[0]);
 } else {
 	readPkgUp().then(x => open(x.pkg.author.name));
